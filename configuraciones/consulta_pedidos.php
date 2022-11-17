a2cf14b2534ec554dcfa4a51ca6340e98d9fed14
Valeria González <71797910+valeria-gonzalez@users.noreply.github.com>
2022-11-16 22:53:43 -0600

    feat: formularios
    En esta rama los formularios ya estan hechos y estan diseñados muy bonis

--- configuraciones/consulta_pedidos.php
+++ configuraciones/consulta_pedidos.php
@@ -39,64 +39,59 @@ $cerrar_conexion = pg_close($conexion);
             <?php $IPATH = $_SERVER["DOCUMENT_ROOT"]."/Jewy_access/cabeceras/"; include($IPATH."header-nav.html"); ?> <!--codigo php usado para incluir el header sin necesidad del codigo-->
                     
             <div class = "main_container" >
-                <div class="item" id = "tabla">
+                <div class="item" id = "consulta-ped">
                     <h3 class="text-center">Coincidencias</h3>
-                    <button type="button" class="boton-personalizado" onclick="location.href='../secciones/vista_pedidos.php'">Atras</button>
                             <tbody>
                                 <?php
                                 if($consulta){
                                     if(pg_num_rows($consulta) > 0){?>
-                        <div class="outer-wrapper">
-                            <div class="table-wrapper">                
-                                <table class="styled-table">
-                                    <thead class="text-muted">
-                                        <th class="text-center">Id</th>
-                                        <th class="text-center">Id Cliente</th>
-                                        <th class="text-center">Fecha del pedido</th>
-                                        <th class="text-center">Fecha de entrega</th>
-                                        <th class="text-center">Productos</th>
-                                        <th class="text-center">Precio</th>
-                                        <th class="text-center">Punto de entrega</th>
-                                        <th class="text-center">Calle</th>
-                                        <th class="text-center">No. de casa</th>
-                                        <th class="text-center">Colonia</th>
-                                        <th class="text-center">Estado</th>
-                                        <th class="text-center">País</th>
-                                        <th class="text-center">Código postal</th>
-                                        <th class="text-center">Referencia</th>
-                                    </thead>
-                                    <tbody>
-                                                <?php
-                                                while($obj=pg_fetch_object($consulta)){?>
-                                        <tr>
-                                            <td><?php echo $obj->id_pedido?></td>
-                                            <td><?php echo $obj->id_cliente?></td>
-                                            <td><?php echo $obj->fecha_pedido?></td>
-                                            <td><?php echo $obj->fecha_entrega?></td>
-                                            <td><?php echo $obj->productos?></td>
-                                            <td><?php echo $obj->precio?></td>
-                                            <td><?php echo $obj->punto_entrega?></td>
-                                            <td><?php echo $obj->calle?></td>
-                                            <td><?php echo $obj->no_casa?></td>
-                                            <td><?php echo $obj->colonia?></td>
-                                            <td><?php echo $obj->estado?></td>
-                                            <td><?php echo $obj->pais?></td>
-                                            <td><?php echo $obj->codigo_postal?></td>
-                                            <td><?php echo $obj->referencia?></td>
-                                        </tr>
-                                        <?php } }
-                                            else{
-                                                echo "<script>
-                                                            alert('No hay coincidencias');
-                                                            history.back();
-                                                        </script>";
-                                            }
-                                    }?>
-                                
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
+                                        <div class="table-responsive table-hover" id="tablaconsulta">
+                        <table class="table">
+                            <thead class="text-muted">
+                                <th class="text-center">Id</th>
+                                <th class="text-center">Id Cliente</th>
+                                <th class="text-center">Fecha del pedido</th>
+                                <th class="text-center">Fecha de entrega</th>
+                                <th class="text-center">Productos</th>
+                                <th class="text-center">Precio</th>
+                                <th class="text-center">Punto de entrega</th>
+                                <th class="text-center">Calle</th>
+                                <th class="text-center">No. de casa</th>
+                                <th class="text-center">Colonia</th>
+                                <th class="text-center">Estado</th>
+                                <th class="text-center">País</th>
+                                <th class="text-center">Código postal</th>
+                                <th class="text-center">Referencia</th>
+                                <th class="text-center">Opciones</th>
+                            </thead>
+                                        <?php
+                                        while($obj=pg_fetch_object($consulta)){?>
+                                <tr>
+                                    <td><?php echo $obj->id_pedido?></td>
+                                    <td><?php echo $obj->id_cliente?></td>
+                                    <td><?php echo $obj->fecha_pedido?></td>
+                                    <td><?php echo $obj->fecha_entrega?></td>
+                                    <td><?php echo $obj->productos?></td>
+                                    <td><?php echo $obj->precio?></td>
+                                    <td><?php echo $obj->punto_entrega?></td>
+                                    <td><?php echo $obj->calle?></td>
+                                    <td><?php echo $obj->no_casa?></td>
+                                    <td><?php echo $obj->colonia?></td>
+                                    <td><?php echo $obj->estado?></td>
+                                    <td><?php echo $obj->pais?></td>
+                                    <td><?php echo $obj->codigo_postal?></td>
+                                    <td><?php echo $obj->referencia?></td>
+                                </tr>
+                                <?php } }
+                                    else{  
+                                            echo "Id no existente.";
+                                        }
+                            }?>
+                            <br>
+                            </tbody>
+                        </table>
+                        <button type="button" onclick="location.href='../secciones/vista_pedidos.php'">Atras</button>
+                    </div>
                     <script src="../js/alerta_eliminar.js"></script>
                 </div> <!-- Fin de item -->
             </div> <!-- Fin de main_container -->
--- configuraciones/mod_productos.php
+++ configuraciones/mod_productos.php
@@ -9,16 +9,14 @@
         <title>Modificar Producto</title>
     </head>
     <body>
+        
         <div class = "wrapper" id = "mod-ped">
             <?php $IPATH = $_SERVER["DOCUMENT_ROOT"]."/Jewy_access/cabeceras/"; include($IPATH."header-nav.html"); ?> <!--codigo php usado para incluir el header sin necesidad del codigo-->
                     
             <div class = "main_container" >
-                <div class="item" id = "mod-ped">
-                    <header>
-                        <h3 class="text-center">Modificar Producto</h3>
-                    </header>
-                    <!--La explixacion del archivo de pedidos podra explicar este, solo agregare mas comentarios en las partes que difieren:D-->
-                    <section> <!--esta seccion contiene el formulario para modificar los datos del producto-->
+                <div class="item" id = "mod-prod">
+                    <div class = "form">
+                        <!--La explixacion del archivo de pedidos podra explicar este, solo agregare mas comentarios en las partes que difieren:D-->
                         <?php 
                             error_reporting(0);
                             include_once '../configuraciones/conexion_bd.php'; //Incluimos la conexion a la base de datos
@@ -26,7 +24,7 @@
 
                             //consultando la lista de Materiales para llenar el cuadro combinado
                             $rsM = pg_query($conexion, "SELECT * FROM material");
-                            
+                                
                             $id = getId(); //obtenemos el id del producto a modificar
 
                             $rs = pg_query($conexion, "SELECT * FROM producto WHERE ID_PRODUCTO = $id"); //obtenemos el registro del producto a modificar
@@ -40,8 +38,8 @@
                                 }
                             else
                                 $producto = pg_fetch_array($rs); //si existe el registro, guardamos el valor de sus columnas en un arreglo
-                                
-                            //Este if es para ver que boton ha presionado el usuario
+                                    
+                                //Este if es para ver que boton ha presionado el usuario
                             if(isset($_POST['btnGenerico'])){ //todos los botones se llaman btnGenerico, por eso se verifica si se ha presionado alguno
                                 $boton = $_POST['btnGenerico']; //guardamos el valor del boton presionado en una variable
 
@@ -54,10 +52,10 @@
 
                                     //implementar la actualizacion de campos en el registro usando las variables
                                     $pg = "UPDATE producto SET NOMBRE = '$nombre', CATEGORIA = '$categoria', 
-                                                            PRECIO = $precio, EXISTENCIA = $existencia 
+                                                                PRECIO = $precio, EXISTENCIA = $existencia 
                                             WHERE ID_PRODUCTO = $id";
 
-                                    //ejecutacion de la sentencia de actualizacion
+                                        //ejecutacion de la sentencia de actualizacion
                                     $rpta = pg_query($conexion, $pg);
 
                                     if($rpta){ //si la actualizacion se realizo correctamente (da verdadero) o no mostramos ventana emergente
@@ -76,7 +74,7 @@
 
                                 if($boton == 'Eliminar todos materiales existentes'){ //este boton elimina todos los materiales existentes
                                     $pg_mat_elim = "DELETE FROM producto_hecho_con 
-                                                        WHERE ID_PRODUCTO = $id";
+                                                    WHERE ID_PRODUCTO = $id";
 
                                     $rpta = pg_query($conexion, $pg_mat_elim);
 
@@ -99,7 +97,7 @@
                                             //con el id_producto que se esta modificando
                                             //y los id_materiales que se seleccionaron
                                             $pg_mat_agr = "INSERT INTO producto_hecho_con 
-                                                            VALUES ($id, $selected)";
+                                                                VALUES ($id, $selected)";
                                             $rpta = pg_query($conexion, $pg_mat_agr);
                                         }
                                     }
@@ -110,8 +108,8 @@
                                             </script>";
                                     }else{
                                         echo '<script> 
-                                                alert("No se pudo agregar los materiales al producto-'.pg_error().'"); 
-                                            </script>';
+                                            alert("No se pudo agregar los materiales al producto-'.pg_error().'"); 
+                                        </script>';
                                     }
                                 }
 
@@ -123,7 +121,7 @@
                                             //con el id_producto que se esta modificando
                                             //y los id_materiales que se seleccionaron
                                             $pg_mat_elim = "DELETE FROM producto_hecho_con 
-                                                            WHERE ID_PRODUCTO = $id AND ID_MATERIAL = $selected"; //se debe eliminar el registro que tenga el id del producto y el id del material
+                                                                WHERE ID_PRODUCTO = $id AND ID_MATERIAL = $selected"; //se debe eliminar el registro que tenga el id del producto y el id del material
                                             $rpta = pg_query($conexion, $pg_mat_elim);
                                         }
                                     }
@@ -140,72 +138,78 @@
                                 }
                             }
                         ?>
-
-                        <form  method = "post">
-                            <!-- campo y boton id -->
-                            <br>
-                            <p><b>Id Producto: <?php echo getId(); ?></b></p>
+                        <div>
+                            <form  method = "post" autocomplete = "off" class = "insert-form">
+                                <div class = "form-heading">
+                                    <h1>Modificar Producto</h1>
+                                    <h2>Id Producto: <?php echo getId(); ?></h2>
+                                    <p>Modifica tú producto llenando los campos, por defecto vienen los valores originales dentro de los campos</p>
+                                </div> <!--end from-heading-->
+                                <!-- campo y boton id -->
+                                <div class = "input-wrap">
+                                    <input type = "text" class = "input-field active" autocomplete = "off" name = "txtNombre" value = "<?php echo $producto[1]; ?>"/>
+                                    <label class = "label">Nombre</label>
+                                </div> <!--end from-heading-->
                                     
-                            <br> <!-- campo y boton de demas campos -->
-                            <label for = "txtNombre">Nombre</label>
-                            <input type = "text" id = "txtNombre" name = "txtNombre" size = "70" value = "<?php echo $producto[1]; ?>"/>
-                        
-                            <br> <br>
-                            <label for = "txtCategoria">Categoria</label> <!-- campo categoria -->
-                            <input type = "text" id = "txtCategoria" name = "txtCategoria" value = "<?php echo $producto[2]; ?>"/>
-                            
-                            <label for = "numPrecio">Precio</label> <!-- campo precio -->
-                            <input type = "number" id = "numPrecio" name = "numPrecio" step = "0.01" value = "<?php echo $producto[3]; ?>"/>
-                    
-                            <br> <br>
-                            <label for = "numExistencia">Existencia</label> <!-- campo existencia -->
-                            <input type = "number" id = "numExistencia" name = "numExistencia" value = "<?php echo $producto[4]; ?>"/>
-                                    
-                            <!--boton para modificar los campos del producto-->
-                            <input type = "submit" name = "btnGenerico" value = "Modificar"/>
-                                    
-                        </form> <!-- fin formulario de demas campos -->
-                    </section> <!-- fin seccion modificar producto -->
+                                <div class = "input-wrap">
+                                    <input type = "text" class = "input-field active" autocomplete = "off" name = "txtCategoria" value = "<?php echo $producto[2]; ?>"/>
+                                    <label class = "label">Categoria</label> <!-- campo categoria -->
+                                </div> <!--end from-heading-->
+                                
+                                <div class = "input-wrap">
+                                    <input type = "text" class = "input-field active" autocomplete = "off" name = "numPrecio" value = "<?php echo $producto[3]; ?>"/>
+                                    <label class = "label">Precio (ej: 0.00)</label> <!-- campo precio -->
+                                </div> <!--end from-heading-->
+                               
+                                <div class = "input-wrap">
+                                    <input type = "text" class = "input-field active" autocomplete = "off"  onfocus = "(this.type = 'number')" 
+                                            onblur = "if(!this.value) this.type = 'text'" name = "numExistencia" value = "<?php echo $producto[4]; ?>"/>
+                                    <label class = "label">Existencia</label> <!-- campo existencia -->
+                                </div> <!--end from-heading-->       
+                                <!--boton para modificar los campos del producto-->
+                                <input type = "submit" name = "btnGenerico" value = "Modificar" class = "submit-btn"/>
+                                        
+                            </form> <!-- fin formulario de demas campos -->
+                        </div> <!-- fin seccion modificar producto -->
 
-                    <section> <!--seccion para modificar los materiales que lleva el producto-->
-                        <br>
-                        <form method = "post">
-                            <br> <!--Separador para indicar que se modificaran los materiales-->
-                            <p><b>Modificar Materiales</b></p>
-                                    
-                            <br> <!--fila para mostrar los materiales que puede llevar producto-->   
-                            <input type = "submit" name = "btnGenerico" value = "Eliminar todos materiales existentes"/>
+                        <div> <!--seccion para modificar los materiales que lleva el producto-->
+                            <form method = "post">
+                                <div class = "form-heading">
+                                    <p>Selecciona los materiales que contendrá después de registrar tú producto, puedes eliminar todos los materiales que tenia, o elegir los materiales a eliminar o agregar</p>
+                                </div> <!--end from-heading-->
+                                        
+                                <input type = "submit" name = "btnGenerico" value = "Eliminar todos materiales existentes" class = "submit-btn" id = "bigger-select"/>
 
-                            <br> <br> <!--select para mostrar los materiales que puede llevar producto-->
-                            <label for = "selMaterial">Escoger materiales</label>
-                            <select name = "selMaterial[]" multiple> <!--multiple indica que se pueden seleccionar varios materiales-->
-                                <option>Escoger material(es)</option>
-                                    <?php 
-                                        //recorriendo por todos los materiales que existen en la bd, rsM es el resultado de la consulta de arriba
-                                        while($material = pg_fetch_array($rsM)){
-                                            echo "<option value = '$material[0]'> $material[1] </option>"; //material[0] es el id (el valor) y material[1] es el nombre (que se muestra en la opcion)
-                                        }      
-                                    ?>
-                            </select>
+                                <!--select para mostrar los materiales que puede llevar producto-->
+                                <select name = "selMaterial[]" multiple class = "select"> <!--multiple indica que se pueden seleccionar varios materiales-->
+                                    <option disabled>Escoger material(es)</option>
+                                        <?php 
+                                            //recorriendo por todos los materiales que existen en la bd, rsM es el resultado de la consulta de arriba
+                                            while($material = pg_fetch_array($rsM)){
+                                                echo "<option value = '$material[0]'> $material[1] </option>"; //material[0] es el id (el valor) y material[1] es el nombre (que se muestra en la opcion)
+                                            }      
+                                        ?>
+                                </select> <br> 
 
-                            <!--boton para agregar los materiales seleccionados-->
-                            <input type = "submit" name = "btnGenerico" value = "Agregar material(es)"/> 
-                            
-                            <!--boton para eliminar los materiales seleccionados-->
-                            <input type = "submit" name = "btnGenerico" value = "Eliminar material(es)"/>
-                                    
-                        </form> <!-- fin formulario de los materiales que puede llevar producto-->
-                    </section> <!-- fin seccion modificar los materiales que lleva el producto-->
+                                <!--boton para agregar los materiales seleccionados-->
+                                <input type = "submit" name = "btnGenerico" value = "Agregar material(es)" class = "submit-btn" id = "select"/> 
+                                
+                                <!--boton para eliminar los materiales seleccionados-->
+                                <input type = "submit" name = "btnGenerico" value = "Eliminar material(es)" class = "submit-btn" id = "select"/>
+                                <button type="button" class = "atras" onclick="location.href='../secciones/vista_productos.php'">Atrás</button>          
+                            </form> <!-- fin formulario de los materiales que puede llevar producto-->
+                        </div> <!-- fin seccion para boton de regresar-->
+                    </div> <!--end form-->
 
-                    <section> <!--seccion para boton de regresar-->
-                        <br> <br>
-                        <a id = "Regresar" href= "../secciones/vista_productos.php" >
-                            <button class = "button">Regresar</button>
-                        </a> 
-                    </section> <!-- fin seccion para boton de regresar-->
+                    <div class = "form-img">
+                        <div class = "illustration">
+                            <img src = "../src/icon-mat.png" alt = "form illustration" />
+                        </div>  <!--end illustration-->
+                    </div> <!--end form-img-->
                 </div> <!-- Fin de item -->
             </div> <!-- Fin de main_container -->
         </div> <!-- Fin de la clase wrapper -->
+        <script src="../js/labels.js"></script> <!--link al archivo js--> 
     </body>
 </html>
 
